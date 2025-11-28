7efc3a452df3c048f627d61bf5dae4e4a3504369
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-11-28 20:37:54 +0200

    Fixed a mistype

--- Bookify.MVC/Contracts/IRoomServices.cs
+++ Bookify.MVC/Contracts/IRoomServices.cs
@@ -6,6 +6,6 @@ namespace Bookify.MVC.Contracts
 {
     public interface IRoomServices
     {
-        Task<List<RoomViewDTO>> GetAllRoomsAsync(int? roomTypeId, RoomStatus? status)
+        Task<List<RoomViewDTO>> GetAllRoomsAsync(int? roomTypeId, RoomStatus? status);
     }
 }
